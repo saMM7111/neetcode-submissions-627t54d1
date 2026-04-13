361cb606abb2aaf9d922f7a743ed3ee1f5ab3cd0
Sankalp Mehta <94738618+saMM7111@users.noreply.github.com>
2026-04-14 04:07:33 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // List<List<String>> result = new ArrayList<>();
+        // boolean [] visited = new boolean[strs.length];
+        
+        // String[] s = new String[strs.length];
+
+        // for(int i = 0;i<strs.length;i++){
+        //     s[i] = sortString(strs[i]);
+        // }
+
+        // for(int i = 0;i<strs.length;i++){
+        //     if(visited[i])continue;
+
+        //     List<String> similar = new ArrayList<>();
+        //     similar.add(strs[i]);
+        //     for(int j = i+1;j<strs.length;j++){
+        //         if(s[i].equals(s[j])){
+        //             similar.add(strs[j]);
+        //             visited[j] = true;
+        //         }
+        //     }
+        //     result.add(similar);
+        // }
+        // return result;
+        HashMap <String, List<String>> map = new HashMap<>();
+        for(int i = 0;i<strs.length;i++){
+            String key = sortString(strs[i]);
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(strs[i]);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public String sortString(String str){
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        String s = new String(chars);
+        return s;
+    }
+}
